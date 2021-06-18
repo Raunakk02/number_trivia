055a213b46392887744382e0ec27b616d48df58a
raunak_k02 <raunakk728@gmail.com>
2021-06-18 23:43:48 +0530

    feat: adds fixture reader and fromJson test for number trivia model

--- test/features/number_trivia/data/number_trivia_model_test.dart
+++ test/features/number_trivia/data/number_trivia_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Text');
@@ -12,4 +16,18 @@ void main() {
       expect(tNumberTriviaModel, isA<NumberTrivia>());
     },
   );
+
+  group('fromJSON', () {
+    test(
+      'should return a valid model when the JSON number is an integer',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+  });
 }
